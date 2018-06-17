f092500c070baf0e0f2f5d4430edf8f3b06b340b
Rhiannon Mortensen <mortensenrhi@gmail.com>
2018-06-16 22:10:19 -0700

    yees

---
+++ ConsoleCalculator/ConsoleCalculator/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ConsoleCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello!");
+
+            Calculator();
+        }
+
+        static void Calculator()
+        {
+           
+        }
+
+    }
+}
